66f3ba1e0953597026149ed252e82e42f9de4c75
Tiago <tiagohucs@gmail.com>
2023-03-19 12:19:36 -0300

    mais aprendizado

--- index.js
+++ index.js
@@ -2,12 +2,10 @@
 var blessed = require('blessed')
 , contrib = require('blessed-contrib')
 , screen = blessed.screen()
-, grid = new contrib.grid({rows: 2, cols: 2, screen: screen})
-
-grid2 = new contrib.grid({rows: 2, cols: 1, screen: screen})
+, grid = new contrib.grid({rows: 4, cols: 2, screen: screen})
 
 // cria grafico linha
-var line = grid.set(0, 0, 1, 1, contrib.line,
+var line = grid.set(0, 0, 2, 1, contrib.line,
 { style:
   { line: "yellow"
   , text: "green"
@@ -30,11 +28,11 @@ var lineData = {
  line.setData([lineData, lineData2])
 
 //cria grafico mapa
-var map = grid.set(0, 1, 1, 1, contrib.map, {label: 'Servers Location'})
+var map = grid.set(0, 1, 2, 1, contrib.map, {label: 'Servers Location'})
 map.addMarker({"lon" : "-79.0000", "lat" : "37.5000", color: "red", char: "X" })
 
 //cria grafico barras
-var bar = grid.set(1, 0, 1, 1, contrib.bar,
+var bar = grid.set(2, 0, 2, 1, contrib.bar,
     { label: 'Server Utilization (%)'
     , barWidth: 4
     , barSpacing: 6
@@ -49,7 +47,7 @@ var barData = {
 bar.setData(barData);
 
 //cria LCD
-var lcd = grid.set(1, 1, 1, 1, contrib.lcd,
+var lcd = grid.set(2, 1, 1, 1, contrib.lcd,
     { segmentWidth: 0.1 // how wide are the segments in % so 50% = 0.5
     , segmentInterval: 0.1 // spacing between the segments in % so 50% = 0.550% = 0.5
     , strokeWidth: 0.11 // spacing between the segments in % so 50% = 0.5
@@ -59,6 +57,21 @@ var lcd = grid.set(1, 1, 1, 1, contrib.lcd,
     , elementPadding: 1 // how far away from the edges to put the elements
     , color: 'red' // color for the segments
     , label: 'Status'})
+
+//cria donut
+var donut = grid.set(3, 1, 1, 1,contrib.donut,{
+	label: 'Test',
+	radius: 8,
+	arcWidth: 3,
+	remainColor: 'black',
+	yPadding: 2,
+	data: [
+	  {percent: 20, label: 'web1', color: 'red'},
+      {percent: 40, label: 'web2', color: 'green'},
+      {percent: 83, label: 'web3', color: 'blue'},
+      {percent: 72, label: 'web4', color: 'white'}
+	]
+  });
 
 
 //teclas de atalho
